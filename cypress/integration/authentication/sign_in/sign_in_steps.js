2c769a0ff41efe4a8a540eefadc9fe4d0551561b
George Sas <georgian@devm8.com>
2020-03-13 20:25:16 +0200

    define first sign in steps with reference to not-yet written methods

---
+++ cypress/integration/authentication/sign_in/sign_in_steps.js
@@ -0,0 +1,32 @@
+import Landing from "../../Pages/Landing";
+import Login from "../../Pages/Login";
+import Account from "../../Pages/Account";
+
+let landingPage = new Landing();
+let loginPage = new Login();
+let accountPage = new Account();
+
+
+Given('I navigate to the authentication page', async () => {
+    landingPage.navigateToTheLoginPage();
+})
+
+
+Given('I input {string} in the Email Address field', email => {
+    cy.get(loginPage.email).type(email);
+})
+
+
+Given('I input {string} in the Password field', password => {
+    cy.get(loginPage.password).type(password);
+})
+
+
+When('I click on the Sign In button', () => {
+    cy.get(loginPage.signIn).click();
+})
+
+
+Then('I am logged in as {string}', user => {
+    accountPage.verifyUserIsLoggedIn(user);
+})
