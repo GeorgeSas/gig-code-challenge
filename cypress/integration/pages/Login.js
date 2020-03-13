38f119483d7663daf0e6c536ef6a3a99358a04a4
George Sas <georgian@devm8.com>
2020-03-13 20:22:22 +0200

    create the login page object

---
+++ cypress/integration/pages/Login.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+export default class Login {
+
+    email = '#email';
+    password = '#passwd'
+    signIn = '#SubmitLogin > span'
+    errorMessage = 'ol > li';
+
+
+}
